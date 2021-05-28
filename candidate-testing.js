5c5ca57b314b77a4fef563140c3d0a16e3ee0531
Paul Thompson <>
2021-05-28 23:02:31 +0000

    changed candidateName to receive input.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = '';
+let candidateName = input.question('What is your name? ');
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
@@ -15,7 +15,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  return candidateName = input.question('What is your name? ');
+  return candidateName;
 }
 
 function askQuestion() {
