4a1fa09131fb27e5d320c46d6e3deca95f3ac9b6
Paul Thompson <67980898+TheCatsPajammies@users.noreply.github.com>
2021-05-29 17:45:18 -0500

    Updated candidateName Variable
    Changed candidateName Variable

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question('What is your name? ');
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
 let correctAnswer = ['Sally Ride', "true", "40", "Trajectory", "3"];
@@ -17,7 +17,7 @@ let testSummary = '';
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  return candidateName;
+  return input.question('What is your name? ');
 }
 
 function askQuestion() {
