d9918d58a4b6bc4c32e47587dda35c783c8e0314
Paul Thompson <>
2021-06-02 04:12:45 +0000

    Trailing spaces

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = '';
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 'What is the minimum crew size for the ISS?'];
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
 let correctAnswers = ['Sally Ride', "true", "40", "Trajectory", "3"];  
 let candidateAnswers = [];
 //let numberCorrect = 0;
@@ -32,8 +32,8 @@ function askQuestion() {
     // while (candidateAnswer !== '') {
     //   candidateAnswer = input.question(question[i]);
     // } !!!
-    // Asks question - formatted using a technique from Gracie.
-    candidateAnswer = input.question(`${questions[i]} `);
+    // Asks question - formatted using a technique from Gracie
+    candidateAnswer = input.question(questions[i]);
     // Stores response
     candidateAnswers.push(String(candidateAnswer));
   }
