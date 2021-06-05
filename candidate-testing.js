48c46108e611f0bbbf9c5348eab5eb4d2ae98b4f
Paul Thompson <>
2021-06-05 16:28:43 +0000

    testing variable format

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,53 +1,48 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-//comment
-// TODO 1.1a: Define candidateName // 
 let candidateName = '';
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+// let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+// let correctAnswers = ['Sally Ride', "true", "40", "Trajectory", "3"];
+
+let questions = ['Who was the first American woman in space? ','True or false: 5000 meters = 5 kilometers. ','(5 + 3)/2 * 10 = ? ','Given the array [8, "Orbit","Trajectory", 45] what entry is at index 2? ','What is the minimum crew size for the International Space Station (ISS)? '];
+let correctAnswers = ['Sally Ride',"true","40","Trajectory","3"];
+
 let correctAnswers = ['Sally Ride', "true", "40", "Trajectory", "3"];  
 let candidateAnswers = [];
-//let numberCorrect = 0;
 let testString = '';
 let testSummary = '';
-//let grade;
 let status;
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question('What is your name? '); 
   return candidateName;
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  // Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses.
+
   for (let i = 0; i < questions.length; i++) {
     
     // !!! Couldn't figure out how to get input validation to prevent empty string submission !!!
     // while (candidateAnswer !== '') {
     //   candidateAnswer = input.question(question[i]);
     // } !!!
-    // Asks question - formatted using a technique from Gracie, however, Jasmine didn't seem to like it so I had to change it back.
     candidateAnswer = input.question(questions[i]);
-    // Stores response
     candidateAnswers.push(String(candidateAnswer));
   }
 }
 
 function gradeQuiz(candidateAnswers) {
   let numberCorrect = 0;
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-  // Lines 45 - 52 = Compare the candidate answers with the correct answers, line 48 is first part of template literal required for the template literal on line 69 for test feedback.
+
   for (let i = 0; i < candidateAnswers.length; i++) {
-    // testString was written to reduce redundancy of code in case extra questions and answers are added to the questions and correctAnswers array variables.
+
     testString += `${i + 1}) ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n\n`;
     
-    // Checking for the correct answer should be case insensitive (e.g. "Orbit" is the same as "orbit").
+
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       numberCorrect += 1;
       //console.log(numberCorrect);
